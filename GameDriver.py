d49da54a24c50ea32be2a0fc4a325a8522abda9d
ErichKramer <erich.kramer1@gmail.com>
2017-05-02 18:42:49 -0700

    Safety push

--- GameDriver.py
+++ GameDriver.py
@@ -5,24 +5,38 @@ import OthelloBoard
 class GameDriver:
     def __init__(self, p1type, p2type, num_rows, num_cols):
         if p1type in "human":
+            4+4
             #make it a human player object
         elif p1type in "minimax" or p1type in "ai":
+            4+4
             #make it a minimax player
         else:
             print("Invalid player 1 type!")
-            exit()
+            exit(-1)
 
         if p2type in "human":
+            4+4
             #make it a human player object
         elif p2type in "minimax" or p1type in "ai":
-            #make it a minimax player
+            4+4#make it a minimax player
         else:
             print("Invalid player 2 type!")
-            exit()
-        
-        self.
+            exit(-1)
+        print("Recieved : ", p1type, " ", p2type);
 
+        self.board = OthelloBoard.OthelloBoard(num_rows, num_cols, 'X', 'O')
+
+    def run(self):
+        print("This is where the game code would go, IF I HAD ANY")
 
 
 def main():
+    if(len(sys.argv)) != 3:
+        print("Usage: python3 GameDriver.py <player1 type> <player2 type>")
+        exit(-1)
+    game = GameDriver(sys.argv[1], sys.argv[2], 4, 4)
+    game.run();
+    return 0
 
+
+main()
--- OthelloBoard.py
+++ OthelloBoard.py
@@ -4,10 +4,26 @@
     If using this code please cite creator.
 
 '''
-import Board
-
-a = Board.Board(5,5);
-a.display()
+from Board import * 
 
 
+
+class OthelloBoard(Board):
+    def __init__(self, rows, cols, p1, p2):
+        Board.__init__(self, rows, cols)
+        self.p1_symbol = p1
+        self.p2_symbol = p2
+
+
+    #this function is substitute for clone. call as New = Old.cloneOBoard()
+    def cloneOBoard(self):
+        tmp = OthelloBoard(self.cols, self.rows, self.p1_symbol, self.p2_symbol)
+        tmp.grid = copy.deepcopy(self.grid)
+        return tmp;
+
+    def initialize(self):
+        Board.set_cell(self.cols /2 -1, self.rows /2 -1, p1_symbol)
+        Board.set_cell(self.cols /2, self.rows /2, p1_symbol)
+        Board.set_cell(self.cols /2 -1, self.rows /2 , p2_symbol)
+        Board.set_cell(self.cols /2, self.rows /2 -1, p2_symbol)
 
